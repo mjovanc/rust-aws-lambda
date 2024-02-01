7dc219256206182080efdccbfbeba78c5442540a
mjovanc <mjovanc@icloud.com>
2024-02-01 08:39:08 +0100

    Updating config

--- terraform/main.tf
+++ terraform/main.tf
@@ -57,7 +57,7 @@ resource "aws_apigatewayv2_stage" "api_stage" {
 
 resource "aws_apigatewayv2_integration" "integration" {
   api_id             = aws_apigatewayv2_api.api.id
-  integration_uri    = aws_lambda_function.rust_lambda_function[0].invoke_arn
+  integration_uri = local.create_iam_role ? aws_lambda_function.rust_lambda_function[0].invoke_arn : null
   integration_method = "POST"
   integration_type   = "AWS_PROXY"
 }
