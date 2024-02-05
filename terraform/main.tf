8947bd36c4dfe791c75e4a46c72664baacf0453b
mjovanc <mjovanc@icloud.com>
2024-02-05 15:12:20 +0100

    Updating main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,33 +6,35 @@ resource "aws_iam_access_key" "lambda-service-user" {
   user = aws_iam_user.lambda-service-user.name
 }
 
-resource "aws_iam_policy" "lambda-service-policy" {
-  name   = "lambda-service-policy"
-  policy = jsonencode({
-    Version   = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "lambda:GetFunction",
-          "lambda:GetLayerVersion",
-          "lambda:CreateFunction",
-          "lambda:UpdateFunctionCode",
-          "lambda:UpdateFunctionConfiguration",
-          "lambda:PublishVersion",
-          "lambda:TagResource"
+resource aws_iam_policy this {
+  name        = format("%s-trigger-transcoder", "test")
+  description = "Allow to access base resources and trigger transcoder"
+  policy      = jsonencode(
+    {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "SomeVeryDefaultAndOpenActions",
+                "Effect": "Allow",
+                "Action": [
+                    "logs:CreateLogGroup",
+                    "ec2:DescribeNetworkInterfaces",
+                    "ec2:CreateNetworkInterface",
+                    "ec2:DeleteNetworkInterface",
+                    "logs:CreateLogStream",
+                    "logs:PutLogEvents"
+                ],
+                "Resource": [
+                    "*"
+                ]
+            }
         ]
-        Resource = [
-          "arn:aws:lambda:<region>:<account-id>:function:<function-name>",
-        ]
-      }
-    ]
-  })
+    })
 }
 
 resource "aws_iam_user_policy_attachment" "lambda-service-user-policy-attachment" {
   user       = aws_iam_user.lambda-service-user.name
-  policy_arn = aws_iam_policy.lambda-service-policy.arn
+  policy_arn = aws_iam_policy.this.arn
 }
 
 output "aws_access_key_id" {
